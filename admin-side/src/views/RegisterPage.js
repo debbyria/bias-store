9f7f6701323ae26dd709e9f97452b205c5152522
unknown <debbyria30@gmail.com>
2022-04-27 02:33:13 +0700

    fix create product form

--- admin-side/src/views/RegisterPage.js
+++ admin-side/src/views/RegisterPage.js
@@ -10,18 +10,18 @@ export default function RegisterPage() {
   return (
     <>
       <div className="min-h-screen flex justify-center items-center bg-white">
-        <div className="p-10 border-[1px] -mt-10 border-slate-200 rounded-md flex flex-col items-center space-y-3">
+        <div className="p-10 border-[1px] -mt-10 border-slate-200 rounded-md flex flex-col items-center space-y-4 w-1/2">
           <div className="py-8">
             Join Us Now
           </div>
-          <div className="flex flex-col space-y-2">
-            <input className="p-3 border-[1px] border-slate-500 rounded-sm w-96" placeholder="Username" type='text'
+          <div className="flex flex-col space-y-4 w-10/12">
+            <input className="p-3 border-[1px] border-slate-500 rounded-md w-full" placeholder="Username" type='text'
               value={username}
               onChange={(e) => {
                 const value = e.target.value
                 setUsername(value)
               }} />
-            <input className="p-3 border-[1px] border-slate-500 rounded-sm w-96" placeholder="Email"
+            <input className="p-3 border-[1px] border-slate-500 rounded-md w-full" placeholder="Email"
               type='text'
               value={email}
               onChange={(e) => {
@@ -29,7 +29,7 @@ export default function RegisterPage() {
                 setEmail(value)
               }}
             />
-            <input className="p-3 border-[1px] border-slate-500 rounded-sm w-96" placeholder="Password"
+            <input className="p-3 border-[1px] border-slate-500 rounded-md w-full" placeholder="Password"
               type='password'
               value={password}
               onChange={(e) => {
@@ -37,7 +37,7 @@ export default function RegisterPage() {
                 setPassword(value)
               }}
             />
-            <input className="p-3 border-[1px] border-slate-500 rounded-sm w-96" placeholder="Phone Number"
+            <input className="p-3 border-[1px] border-slate-500 rounded-md w-full" placeholder="Phone Number"
               type='text'
               value={phoneNumber}
               onChange={(e) => {
@@ -45,7 +45,7 @@ export default function RegisterPage() {
                 setPhoneNumber(value)
               }}
             />
-            <input className="p-3 border-[1px] border-slate-500 rounded-sm w-96" placeholder="Address"
+            <input className="p-3 border-[1px] border-slate-500 rounded-md w-full" placeholder="Address"
               type='text'
               value={address}
               onChange={(e) => {
@@ -54,7 +54,7 @@ export default function RegisterPage() {
               }}
             />
           </div>
-          <div className="flex flex-col space-y-5 w-full">
+          <div className="flex flex-col space-y-5 w-10/12">
             <button className="w-full bg-[#0070ba] rounded-3xl p-3 text-white font-bold transition duration-200 hover:bg-[#003087]">Sign Up</button>
           </div>
         </div>
