1b2af1bb53675d08aabe10c2a612b8912f68f637
unknown <debbyria30@gmail.com>
2022-04-30 13:41:56 +0700

    add helper function jwt, bcryptjs and generate slug

---
+++ server/seeders/20220430062436-seed-user.js
@@ -0,0 +1,26 @@
+'use strict';
+
+let data = require('../dummy/user.json')
+console.log(data)
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
