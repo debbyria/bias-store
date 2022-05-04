7f84efefe87c73b8071d8316893f45011f00a6ea
unknown <debbyria30@gmail.com>
2022-05-04 15:36:45 +0700

    add testimonial content

--- client-side/src/views/LandingPage.js
+++ client-side/src/views/LandingPage.js
@@ -1,11 +1,13 @@
 import HeaderContent from "../components/HeaderContent"
 import FooterContent from "../components/FooterContent"
 import NewsContent from "../components/NewsContent"
+import TestimonialContent from "../components/TestimonialContent"
 
 export default function LandingPage() {
   return (
     <>
       <HeaderContent />
+      <TestimonialContent />
       <NewsContent />
       <FooterContent />
     </>
